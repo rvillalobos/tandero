f0fab37fb0473241cec11e59e99fb11a21fed1f7
CharlyFonseca <is688302@iteso.mx>
2014-11-11 23:40:14 -0600

    Created new Methods and Classes, Updated older methods and Classes

--- src/main/java/com/iteso/sweng/Tanda/Registry.java
+++ src/main/java/com/iteso/sweng/Tanda/Registry.java
@@ -1,7 +1,38 @@
 package com.iteso.sweng.Tanda;
 
+import java.util.ArrayList;
+
+import static com.iteso.sweng.Tanda.Organizer.becomeAnOrganizer;
+
 /**
  * Created by Charly on 11/11/2014.
  */
 public class Registry {
+
+    private static ArrayList<User> newUsers = new ArrayList<User>();
+    private static ArrayList<Organizer> newOrganizers = new ArrayList<Organizer>();
+
+    public static void registerNewUser (String name){
+    User userToRegister = new User(name);
+        if(newUsers.contains(userToRegister)){
+            System.out.println("User already exists!");
+        }else {
+            newUsers.add(userToRegister);
+            System.out.println("User created successfully");
+        }
+    }
+
+    public static void createNewOrganizer (String name) {
+        User userToOrganizer = new User(name);
+            if(newUsers.contains(userToOrganizer)){
+                if(newOrganizers.contains(becomeAnOrganizer(userToOrganizer))){
+                    System.out.println("User is already an Organizer!");
+                }else {
+                    newOrganizers.add(becomeAnOrganizer(userToOrganizer));
+                }
+            }else{
+                System.out.println("User not Found");
+            }
+    }
+
 }
