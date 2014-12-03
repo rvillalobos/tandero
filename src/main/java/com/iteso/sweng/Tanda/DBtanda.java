6a92e66f53d9121903e756d98f5735f18d84a8bb
CoCoNO <cocono57@hotmail.com>
2014-12-03 13:41:24 -0600

    List of active tandas
    Added tanda and tandapartaker table

---
+++ src/main/java/com/iteso/sweng/Tanda/DBtanda.java
@@ -0,0 +1,51 @@
+package com.iteso.sweng.Tanda;
+
+import com.iteso.sweng.RecoveryPass.SendEmail;
+import com.iteso.sweng.SignIn.SignInEmail;
+import java.sql.*;
+
+/**
+ * Created by Daniel on 12/10/14.
+ */
+public class DBtanda {
+
+    public Connection conexion;
+    public String userDb = "root";
+    public String passDb = "root";
+
+    public void conectar() throws SQLException,ClassNotFoundException{
+        Class.forName("com.mysql.jdbc.Driver");
+        conexion=DriverManager.getConnection("jdbc:mysql://localhost:3306/tandero",userDb, passDb);
+    }
+    //Desconectar a la Base de datos
+    public void desconectar() throws SQLException, ClassNotFoundException{
+        conexion.close();
+    }
+	ResultSet rs;
+    public void loadTandas() throws SQLException{
+        String sql = "SELECT * FROM tanda";
+        PreparedStatement ps = conexion.prepareStatement(sql);
+        rs = ps.executeQuery();
+	}
+	//getNextTanda return containter tandal
+	public iTanda getNextTanda() throws SQLException
+	{
+		if(rs==null)return null;
+		iTanda n=null;
+		if(rs.next()){
+		n=new iTanda();
+		n.name = rs.getString("Name");
+		n.state = rs.getInt("state");
+		n.userId=rs.getInt("organizer");
+		
+		//Get oranizer name
+		String sql = "SELECT * FROM User WHERE iduser = "+n.userId ;
+        PreparedStatement ps = conexion.prepareStatement(sql);
+        ResultSet userSet = ps.executeQuery();
+		if(userSet.next())
+			n.userName=userSet.getString("name");
+		
+		}
+		return n;
+	}
+}
---
+++ src/main/java/com/iteso/sweng/Tanda/iTanda.java
@@ -0,0 +1,14 @@
+package com.iteso.sweng.Tanda;
+
+import com.iteso.sweng.RecoveryPass.SendEmail;
+import com.iteso.sweng.SignIn.SignInEmail;
+import java.sql.*;
+
+
+public class iTanda {
+
+public String name;
+public int state;
+public int userId;
+public String userName;
+}
